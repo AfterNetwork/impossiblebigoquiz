42803f79786160c86ec2ad507792810391ca23ae
Seth Koch <sethkoch1@gmail.com>
2016-11-09 09:51:44 -0600

    Got the npm watch working, seperated the controller from the module, created a bigo folder to hold the bigo controller and temaple

--- client/dist/appmodule/app.js
+++ client/dist/appmodule/app.js
@@ -1,15 +1,16 @@
 'use strict';
 
-//asdf
-
-var routerApp = angular.module('routerApp', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
+//asasdfasdfasdfasdf
+angular.module('quizApp', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise('/quiz');
 
-    $stateProvider.state('quiz', {
+    $stateProvider
+
+    //The Big O Quiz
+    .state('quiz', {
         url: '/quiz',
-        templateUrl: '../../templates/questions/index.html',
-        controller: 'MainpageController'
+        templateUrl: '../../templates/bigo/questions/index.html'
     }).state('victory', {
         url: '/victory',
         templateUrl: '../../templates/victory/victory.html'
@@ -17,40 +18,4 @@ var routerApp = angular.module('routerApp', ['ui.router']).config(function ($sta
         url: '/death',
         templateUrl: '../../templates/death/death.html'
     });
-});
-
-routerApp.controller('MainpageController', function ($http, $state, $sce) {
-    var _this = this;
-
-    this.allTheQuestions;
-    this.question;
-    $http.post('/questions').then(function (res) {
-        _this.allTheQuestions = res.data;
-        _this.question = $sce.trustAsHtml(_this.allTheQuestions[_this.counter].question);
-    });
-    this.victory = 0;
-    this.counter = 0;
-    this.answer;
-    this.text = '';
-    this.submit = function () {
-        this.answer = this.text;
-        this.text = '';
-        if (this.answer === this.allTheQuestions[this.counter].answer) {
-            this.counter++;
-            this.victory++;
-            if (this.victory === 5) {
-                $state.go('victory');
-                this.counter = 0;
-                this.question = $sce.trustAsHtml(this.allTheQuestions[this.counter].question);
-                this.victory = 0;
-            } else {
-                this.question = $sce.trustAsHtml(this.allTheQuestions[this.counter].question);
-            }
-        } else {
-            this.victory = 0;
-            this.counter = 0;
-            this.question = $sce.trustAsHtml(this.allTheQuestions[this.counter].question);
-            $state.go('death');
-        }
-    };
 });
