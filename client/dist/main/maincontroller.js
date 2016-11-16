3d0763dd7635c464208e686633379ea2a1a95d24
Seth Koch <sethkoch1@gmail.com>
2016-11-16 17:28:59 -0600

    connected both the homepage view and the death view to the maincontroller and added a post call to that so that if the user is not signed in and tries to access one of those pages they will be redirected to signin

--- client/dist/main/maincontroller.js
+++ client/dist/main/maincontroller.js
@@ -1,7 +1,9 @@
 'use strict';
 
-angular.module('quizApp').controller('MainController', function ($window) {
-
+angular.module('quizApp').controller('MainController', function ($http, $state, $window) {
+  $http.post('/questions', { token: $window.localStorage.accessToken }).then(function (res) {}, function (res) {
+    $state.go('signin');
+  });
   this.out = function () {
 
     $window.localStorage.accessToken = '';
--- client/src/main/maincontroller.js
+++ client/src/main/maincontroller.js
@@ -1,6 +1,11 @@
 angular.module('quizApp')
-  .controller('MainController', function($window){
+  .controller('MainController', function($http, $state, $window){
+    $http.post('/questions', {token:$window.localStorage.accessToken})
+      .then((res) => {
 
+      },(res) => {
+        $state.go('signin');
+      });
     this.out = function(){
 
             $window.localStorage.accessToken = '';
