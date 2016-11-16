a755081dfef6deb40eb9563446ed11922b5e79ff
Seth Koch <sethkoch1@gmail.com>
2016-11-16 15:14:16 -0600

    made landing page the sign page

--- client/src/appmodule/app.js
+++ client/src/appmodule/app.js
@@ -4,7 +4,7 @@ angular.module('quizApp', ['ui.router'])
 
   .config(function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/signin');
 
     $stateProvider
 
