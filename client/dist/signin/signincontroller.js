61aa776486b04a02ee8b90d7cab29f938ba913bf
Seth Koch <sethkoch1@gmail.com>
2016-11-19 08:09:06 -0600

    made a lot of small style changes.  On a big note, got the body background to switch after signing in, using a ternary operator in the body tag of index.html

--- client/dist/signin/signincontroller.js
+++ client/dist/signin/signincontroller.js
@@ -1,18 +1,22 @@
 'use strict';
 
-angular.module('quizApp').controller('SignInController', function ($http, $state, $window) {
+angular.module('quizApp').controller('SignInController', function ($http, $state, $window, $rootScope) {
   this.user;
   this.password;
   this.token = "";
+
   $http.post('/questions', { token: $window.localStorage.accessToken }).then(function (res) {
     $state.go('home');
+    $rootScope.bg = false;
   }, function (res) {});
   this.submit = function () {
     $http.post('/authenticate', { username: this.user, password: this.password }).then(function (res) {
       $window.localStorage.accessToken = res.data.token;
+      $rootScope.bg = false;
       $state.go('home');
     });
     this.user = '';
     this.password = '';
   };
+  console.log($rootScope.bg);
 });
