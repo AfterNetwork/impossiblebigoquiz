1b3f0705d5a81aeab9f402ce500ecb16a92f6fa4
Seth Koch <sethkoch1@gmail.com>
2016-11-25 15:26:05 -0600

    message in commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# impossiblebigoquiz
-The impossible big o quiz
+# constcodeprep
+Const Code Prep
+
+This repository is not complete.  An essential file has been added to the gitignore.  It holds the tranporter for the mailer, and the transporter contains sensitive data, password, that I do not want on github.
--- server/mailer.js
+++ server/mailer.js
@@ -1,20 +1,29 @@
-var nodemailer = require('nodemailer');
-var secrets = require('./secrets.js');
-var transporter = secrets.transporter;
+// var nodemailer = require('nodemailer');
+// var secrets = require('./secrets.js');
+// var transporter = secrets.transporter;
 
 
 
-var mailOptions = {
-    from: '"Seth Koch ?" <constcodeprepmailer@gmail.com>', // sender address
-    to: 'sethkoch1@gmail.com', // list of receivers
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world ?', // plaintext body
-    html: '<b>Hello world ?</b>' // html body
-};
 
-transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        return console.log(error);
-    }
-    console.log('Message sent: ' + info.response);
-});
+
+
+
+// var mailOptions = {
+//     from: '"Seth Koch ?" <constcodeprepmailer@gmail.com>', // sender address
+//     to: email, // list of receivers
+//     subject: 'Hello ✔', // Subject line
+//     text: 'Hello world ?', // plaintext body
+//     html: '<b>Hello world ?</b>' // html body
+// };
+
+
+// var signUpMail = () => {
+//   transporter.sendMail(mailOptions, function(error, info){
+//       if(error){
+//           return console.log(error);
+//       }
+//       console.log('Message sent: ' + info.response);
+//   });
+// };
+
+// exports.signUpMail = signUpMail;
