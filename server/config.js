d58b1c9380ef06f28ce0444bd24ecc0f6a485392
Seth Koch <sethkoch1@gmail.com>
2016-11-25 17:08:48 -0600

    hid password in a file that I will not push to github, set password as an environment variable on heroku, this is the password for the email to use for the transporter

--- server/config.js
+++ server/config.js
@@ -1,5 +1,8 @@
+var secrets = require('./secretvars.js');
+var passcode = secrets.passcode;
 module.exports ={
   'secret' : 'silence',
-  'database': process.env.MONGODB_URI || 'mongodb://localhost/quicktest'
+  'database': process.env.MONGODB_URI || 'mongodb://localhost/quicktest',
+  'passcode': process.env.PASSCODE || passcode
 
 }
--- server/secrets.js
+++
@@ -1,5 +0,0 @@
-var nodemailer = require('nodemailer');
-
-var transporter = nodemailer.createTransport('smtps://constcodeprepmailer@gmail.com:FF7765fdp@smtp.gmail.com');
-
-exports.transporter = transporter;
