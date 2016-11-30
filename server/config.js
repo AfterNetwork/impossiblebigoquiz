85a6725b524bf3619312abdeaa8a6d05611c44df
Seth Koch <sethkoch1@gmail.com>
2016-11-30 11:50:01 -0600

    asdf

--- server/config.js
+++ server/config.js
@@ -1,10 +1,10 @@
-if(!process.env.Passcode){
-  var secrets = require('./secretpass.js');
-  var passcode = secrets.passcode;
-}
+// if(!process.env.Passcode){
+//   var secrets = require('./secretpass.js');
+//   var passcode = secrets.passcode;
+// }
 module.exports ={
   'secret' : 'silence',
   'database': process.env.MONGODB_URI || 'mongodb://localhost/quicktest',
-  'passcode': process.env.PASSCODE || passcode
+  'passcode': process.env.PASSCODE
 
 }
