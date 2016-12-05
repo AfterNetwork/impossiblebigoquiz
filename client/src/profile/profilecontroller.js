2da58b1a25e6db84f5cd8da08ba1a38e90ced486
Seth Koch <sethkoch1@gmail.com>
2016-12-05 13:01:16 -0600

    made JWT secret a heroku variable

--- client/src/profile/profilecontroller.js
+++ client/src/profile/profilecontroller.js
@@ -1,6 +1,8 @@
 angular.module('quizApp')
   .controller('ProfileController',["$rootScope", "getUserName", "authCheck", function($rootScope, getUserName, authCheck){
+    //make sure user is authorized
     authCheck.auth();
+    //sets current user name to the root scope currentUser
     getUserName.getUser();
 
   }]);
--- server/config.js
+++ server/config.js
@@ -1,8 +1,9 @@
+// sets secrets to either be the heroku variable or, if not accessing through heroku, will be set through a file which is on the project but not pushed to github or heroku.
 var secrets = process.env.PASSCODE === undefined ? require('./secretpass.js') : 'test';
 var passcode = secrets.passcode;
 
 module.exports ={
-  'secret' : 'silence',
+  'secret' : process.env.SECRET || 'happiness',
   'database': process.env.MONGODB_URI || 'mongodb://localhost/quicktest',
   'passcode': process.env.PASSCODE || passcode
 
